0f925b538f2e86f0916a4bb9ef3ec5500817d16c
mtoro64 <toromanuel64@gmail.com>
2017-05-05 14:35:20 -0700

    undo edit

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ class Main
   public static void main(String[] args)
   {
     System.out.println( "This is a new file" );
-    System.out.println( "Edit to original" );
   }//end main method
   
 }//end class main
